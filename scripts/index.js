a77ecd93be09ccc0ae040fe53801ab42bb2df93d
Julia Gulla <julia1603@gmail.com>
2024-03-01 11:58:36 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function renderCard(item, deleteButton) {
+    const card = document.querySelector('#card-template').content;
+    const cardElement = card.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteButton(cardElement)});
+    return cardElement;
+    }
+    
+function deleteButton(cardElement) {
+    cardElement.remove();
+}
+initialCards.forEach(function(item){
+   placesList.append(renderCard(item, deleteButton));
+});
